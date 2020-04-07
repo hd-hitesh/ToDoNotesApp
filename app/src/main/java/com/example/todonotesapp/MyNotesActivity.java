c6c0c415b1d5ac036317b2ba9755ea82d89447f7
hd-hitesh <hdhitesh11@gmail.com>
2020-04-07 13:26:01 +0530

    Second Commit

--- app/src/main/java/com/example/todonotesapp/MyNotesActivity.java
+++ app/src/main/java/com/example/todonotesapp/MyNotesActivity.java
@@ -1,25 +1,70 @@
 package com.example.todonotesapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MyNotesActivity extends AppCompatActivity {
 
     String fullName;
+    FloatingActionButton fabAddNotes;
+    TextView textViewTitle, textViewDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notes);
+        fabAddNotes = findViewById(R.id.fabAddNotes);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewDescription = findViewById(R.id.textViewDescription);
 
         Intent intent = getIntent();
         intent.getStringExtra("full_name");
         fullName = intent.getStringExtra("full_name");
 
+        fabAddNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Log.d("MyNotesActivity","On click performed");
+                setUpDialogBox();
+            }
+        });
+
        // Log.d("MyNotesActivity",fullName);
         getSupportActionBar().setTitle(fullName);
 
+    }
+
+    private void setUpDialogBox() {
+        View view = LayoutInflater.from(MyNotesActivity.this).inflate(R.layout.add_notes_dialog_layout,null);
+        final EditText editTextTitle = view.findViewById(R.id.editTextTitle);
+        final EditText editTextDescription = view.findViewById(R.id.editTextDesciption);
+        Button buttonSubmit = view.findViewById(R.id.buttonSubmit);
+        //Dialog
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setView(view)
+                .setCancelable(false)
+                .create();
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textViewTitle.setText(editTextTitle.getText().toString());
+                textViewDescription.setText(editTextDescription.getText().toString());
+                dialog.hide();
+
+            }
+        });
+        dialog.show();
     }
 }
