88618901607b853ed31c9f304ea5944f3feecae6
hd-hitesh <hdhitesh11@gmail.com>
2020-04-11 13:59:35 +0530

    Notification Services

--- app/src/main/java/com/example/todonotesapp/view/SplashActivity.kt
+++ app/src/main/java/com/example/todonotesapp/view/SplashActivity.kt
@@ -3,9 +3,13 @@ package com.example.todonotesapp.view
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todonotesapp.utils.PrefConstant
 import com.example.todonotesapp.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 
 class SplashActivity : AppCompatActivity() {
 
@@ -16,6 +20,24 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
         setUpSharedPreferences()
         checkLoginStatus()
+        getFCMToken()
+    }
+
+    private fun getFCMToken() {
+        FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w("SplashActivity", "getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+                    Log.d("SplashActivity", token)
+                    Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+                })
     }
 
     private fun setUpSharedPreferences() {
